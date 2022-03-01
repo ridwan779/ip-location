f2220e188efe9220454beabc90ae212f1f3cdd37
Ridwan Alamsyah <ridwan@webarq.co.id>
2022-03-01 12:33:36 +0700

    penambahan dockerfile

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:16.13-alpine3.12
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install timezone
+RUN apk update && apk add tzdata
+ENV TZ="Asia/Jakarta"
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json ./
+
+RUN npm install
+# If you are building your code for production
+# RUN npm ci --only=production
+
+# Bundle app source
+COPY . .
+
+CMD [ "npm", "run", "start" ]
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 let express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send("Hello World");
+app.get('/check-ip', function (req, res) {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+    console.log(ip);
+    res.json({
+        ip: ip
+    });
 });
 
-app.listen(3000, function () {
-    console.log('Listening on port 3000')
+app.listen(process.env.APP_PORT, function () {
+    console.log('Listening on port '+ process.env.APP_PORT)
 });
