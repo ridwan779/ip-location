089f75050f31d552a0dc47540e65a8c4704b7b10
Ridwan Alamsyah <ridwan@webarq.co.id>
2022-03-10 13:58:46 +0700

    test triggers

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/check-ip', function (req, res) {
-    console.log('test2');
+    console.log('test1');
     var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
     console.log(ip);
     res.json({
